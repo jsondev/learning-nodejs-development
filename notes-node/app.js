d92cc97716a9f08cb1e77818a1c248b5b56a2f7e
James Anderson <jsondev1.0@outlook.com>
2018-03-11 23:38:38 -0400

    removing duplicate indexed values

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,6 +1,7 @@
 console.log('Starting app');
 const fs = require('fs');
 const os = require('os');
+const _= require('lodash');
 const notes = require('./notes');
 
 var user = os.userInfo();
@@ -16,4 +17,9 @@ var res = notes.addNote();
     }
 }); */
 
-console.log('Result:', notes.add(2,3));
+// console.log('Result:', notes.add(2,3));
+
+console.log(_.isString(true));
+console.log(_.isString('James'));
+var filteredArray = _.uniq(['James', 1, 'James' , 1,2,3,4]);
+console.log(filteredArray);
