6271a67112a87919b99d1af6b76233213a022d82
James Anderson <jsondev1.0@outlook.com>
2018-03-11 23:46:47 -0400

    cleaning up the app and removing dummy code

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,25 +1,6 @@
 console.log('Starting app');
+
 const fs = require('fs');
-const os = require('os');
 const _= require('lodash');
+
 const notes = require('./notes');
-
-var user = os.userInfo();
-var res = notes.addNote();
-
-
-/* fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err, data){
-    if (err){
-        console.log("File created unsuccessfully");
-    }
-    else {
-        console.log("File created successfully");
-    }
-}); */
-
-// console.log('Result:', notes.add(2,3));
-
-console.log(_.isString(true));
-console.log(_.isString('James'));
-var filteredArray = _.uniq(['James', 1, 'James' , 1,2,3,4]);
-console.log(filteredArray);
