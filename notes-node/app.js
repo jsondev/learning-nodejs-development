c23906da9632f12e0313784d357837ae6ea2584f
James Anderson <jsondev1.0@outlook.com>
2018-03-11 23:07:51 -0400

    mathmatical!

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,14 +4,16 @@ const os = require('os');
 const notes = require('./notes');
 
 var user = os.userInfo();
+var res = notes.addNote();
 
-console.log(user.username);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err, data){
+/* fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err, data){
     if (err){
         console.log("File created unsuccessfully");
     }
     else {
         console.log("File created successfully");
     }
-});
+}); */
+
+console.log('Result:', notes.add(2,3));
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,3 +1,12 @@
 console.log('Starting notes.js');
 
 module.exports.age = 31;
+
+module.exports.addNote = () => {
+console.log('addNote');
+return 'New note';
+}
+
+module.exports.add = (a,b) => {
+    return a + b;
+}
