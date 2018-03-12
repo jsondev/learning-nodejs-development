56ead3b87a2e754a923ac902f171f9d10d73fd06
James Anderson <jsondev1.0@outlook.com>
2018-03-11 22:56:42 -0400

    adding age module export

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,12 +1,13 @@
 console.log('Starting app');
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes');
 
 var user = os.userInfo();
 
 console.log(user.username);
 
-fs.appendFile('greetings.txt', `Hello ${user.username}!`, function(err, data){
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}`, function(err, data){
     if (err){
         console.log("File created unsuccessfully");
     }
