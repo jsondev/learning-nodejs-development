3594399de709e2a7353dbbd4e3a3b3f558c05917
James Anderson <jsondev1.0@outlook.com>
2018-03-12 16:48:23 -0400

    adding yargs

--- notes-node/app.js
+++ notes-node/app.js
@@ -4,3 +4,25 @@ const fs = require('fs');
 const _= require('lodash');
 
 const notes = require('./notes');
+
+console.log(process.argv);
+
+var command = process.argv[2];
+
+
+switch (command){
+    case 'add':
+    console.log('Adding new note');
+    break;
+    case 'list':
+    console.log('Listing all notes');
+    break;
+    case 'read':
+    console.log('Fetching notes');
+    break;
+    case 'remove':
+    console.log('Removing note');
+    break;
+    default:
+    console.log("Command not recognized");
+}
