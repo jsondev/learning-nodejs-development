3568d108951211ebc8607c8d2656559a1d836be2
James Anderson <jsondev1.0@outlook.com>
2018-03-11 22:14:11 -0400

    creating a file with fs

--- notes-node/app.js
+++ notes-node/app.js
@@ -1,1 +1,11 @@
 console.log('Starting app');
+const fs = require('fs');
+
+fs.appendFile('greetings.txt', 'Hello World', function(err, data){
+    if (err){
+        console.log("File created unsuccessfully");
+    }
+    else {
+        console.log("File created successfully");
+    }
+});
