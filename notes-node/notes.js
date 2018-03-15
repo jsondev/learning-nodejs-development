25cd4d04bf3d68960d46f79cfdd44dbc0093e79c
James Anderson <jsondev1.0@outlook.com>
2018-03-14 20:58:35 -0400

    updating add command to build an array

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,21 +1,32 @@
-module.exports.age = 31;
+var fs = require('fs');
 
 var addNote = (title, body) => {
     console.log('Adding note', title, body);
+    var notesString = fs.readFileSync('notes-data.json');
+    var notes = JSON.parse(notesString);
+    var note = {
+        title,
+        body
+    };
+    
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+
+
 }
 var getAll = () => {
     console.log('Getting all notes');
 }
 var getNote = (title) => {
- console.log('Getting note:' , title);
+    console.log('Getting note:', title);
 }
 var removeNote = (title) => {
     console.log('Removing note', title);
-   };
+};
 
 module.exports = {
- addNote,
- getAll,
- getNote,
- removeNote
+    addNote,
+    getAll,
+    getNote,
+    removeNote
 }
