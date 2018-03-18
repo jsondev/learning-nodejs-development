e51bf462743a0462f402ca3782eb031d5a05b5ae
James Anderson <jsondev1.0@outlook.com>
2018-03-17 21:00:12 -0400

    adding reading note command

--- notes-node/app.js
+++ notes-node/app.js
@@ -3,38 +3,47 @@ const chalk = require('chalk');
 console.log('Starting app');
 
 const fs = require('fs');
-const _= require('lodash');
+const _ = require('lodash');
 const yargs = require('yargs');
 
 const notes = require('./notes');
 const argv = yargs.argv;
 var command = argv._[0];
 
-switch (command){
+switch (command) {
     case 'add':
-    var note = notes.addNote(argv.title, argv.body);
-    if (note){
-        console.log('Note created');
-        console.log('--');
-        console.log(`Title: ${note.title}`);
-        console.log(`Body: ${note.body}`);
-       
-    }
-    else {
-        console.log(`Note title taken`);
-    }
-    break;
+        var note = notes.addNote(argv.title, argv.body);
+        if (note) {
+            console.log('Note created');
+            console.log('--');
+            console.log(`Title: ${note.title}`);
+            console.log(`Body: ${note.body}`);
+
+        }
+        else {
+            console.log(`Note title taken`);
+        }
+        break;
     case 'list':
-    notes.getAll();
-    break;
+        notes.getAll();
+        break;
     case 'read':
-    notes.getNote(argv.title);
-    break;
+        notes.getNote(argv.title);
+        var note = notes.getNote(argv.title);
+        if (note) {
+            console.log('Note found');
+            console.log('--');
+            console.log(`Title: ${note.title}`);
+            console.log(`Body: ${note.body}`);
+        } else {
+            console.log('Note not found');
+        }
+            break;
     case 'remove':
-    var noteRemoved = notes.removeNote(argv.title);
-    var message = noteRemoved ? 'Note was removed' : 'Note not found';
-    console.log(message);
-    break;
+        var noteRemoved = notes.removeNote(argv.title);
+        var message = noteRemoved ? 'Note was removed' : 'Note not found';
+        console.log(message);
+        break;
     default:
-    console.log("Command not recognized");
+        console.log("Command not recognized");
 }
--- notes-node/notes.js
+++ notes-node/notes.js
@@ -37,7 +37,11 @@ var getAll = () => {
     console.log('Getting all notes');
 }
 var getNote = (title) => {
-    console.log('Getting note:', title);
+
+    var notes = fetchNotes();
+    var filteredNotes = notes.filter((note) => note.title === title);
+    return filteredNotes[0];
+    
 }
 var removeNote = (title) => {
     var notes = fetchNotes();
