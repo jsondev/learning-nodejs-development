b8fb7c100ee3a7300084c1b6b2e2658bc09c1c27
James Anderson <jsondev1.0@outlook.com>
2018-03-18 17:52:00 -0400

    adding the weather app, required request module

---
+++ weather-app/app.js
@@ -0,0 +1,9 @@
+const request = require('request');
+
+
+request({
+    url:
+   'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%2
+   0street%20philadelphia',
+   }, (error, response, body) => {
+   });
