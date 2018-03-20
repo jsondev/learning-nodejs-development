6a5197f8f20516d18551eab8f7ffb380dee2eb71
James Anderson <jsondev1.0@outlook.com>
2018-03-19 22:58:38 -0400

    added yargs, address options, and template for encodeAddress

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,26 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs.options({
+  a: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+}).help().alias('help', 'h').argv;
+var encodeAddress = encodeURIComponent(argv.address);
+
+console.log(argv);
 
 request({
-    url:
-   'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%2
-   0street%20philadelphia',
-   }, (error, response, body) => {
-   });
+  url:
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeAddress}`,
+  json: true
+}, (error, response, body) => {
+  console.log(`Address: ${body.results[0].formatted_address}`);
+  console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
+
+
+});
