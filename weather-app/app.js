aa52ffd36addc345a7e1875a8bbfa41cf905efde
James Anderson <jsondev1.0@outlook.com>
2018-03-21 00:23:55 -0400

    adding forcast.io api

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,18 @@
 const yargs = require('yargs');
 const geocode = require('./geocode/geocode');
+const request = require('request');
 
-const argv = yargs.options({
+request({
+  url: 'https://api.darksky.net/forecast/a85eccd95ea60eb2749e9da73b29b353/38.8594866,-77.3527744',
+  json:true
+}, (error, response, body)=> {
+  if (!error && response.statusCode === 200) {
+    console.log(body.currently.temperature);   
+}
+});
+
+
+/* const argv = yargs.options({
   a: {
     demand: true,
     alias: 'address',
@@ -16,6 +27,6 @@ geocode.geocodeAddress(argv.address, (errorMessage, results) => {
   } else {
   console.log(JSON.stringify(results, undefined, 2));
   }
- });
+ }); */
  
 
