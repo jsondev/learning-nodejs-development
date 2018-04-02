2d2cc6cee6a6503a5d45e4d724b061d86a11379d
James Anderson <jsondev1.0@outlook.com>
2018-04-02 19:29:17 -0400

    adding serve directory

---
+++ node-web-server/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.send({
+        name: 'Andrew',
+        likes: [
+            'Biking',
+            'Cities'
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Page');
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    })
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Server is up and running on port 3000');
+});
