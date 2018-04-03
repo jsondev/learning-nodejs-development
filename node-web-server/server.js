d3c2b01031725d078533fda68b537cc6f030c064
James Anderson <jsondev1.0@outlook.com>
2018-04-03 08:56:24 -0400

    finishing up maintenance section

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -5,18 +5,28 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
-app.use((req, res,next)=>{
+
+
+// Server connection logs
+app.use((req, res, next) => {
     var now = new Date().toString();
     var log = `${now} : ${req.method}  ${req.url}`
-    fs.appendFile('server.log', log + '\n', (err)=>{
+    fs.appendFile('server.log', log + '\n', (err) => {
         if (err) {
             console.log('Unable to append to server.log');
         }
     });
     console.log(log);
-next();
+    next();
 });
+
+app.use((req, res, next) => {
+    res.render('maintenance.hbs');
+    
+})
+app.use(express.static(__dirname + '/public'));
+
+// Helpers
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
@@ -24,6 +34,7 @@ hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 })
 
+// Routes
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
@@ -44,7 +55,7 @@ app.get('/bad', (req, res) => {
     })
 });
 
-
+// Initializing server
 app.listen(3000, () => {
     console.log('Server is up and running on port 3000');
 });
